6981ff3c311b81370b154bf7285ee57d429b3ac6
Rubén Chiquin <randreu28@gmail.com>
2023-04-11 17:04:29 +0200

    Adds a signature

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import { Canvas } from "@react-three/fiber";
 import { Suspense } from "react";
 import Shader from "./components/Shader";
 import Loading from "./components/Loading";
+import Signature from "./components/Signature";
 
 export default function App() {
   return (
     <>
       <Suspense fallback={<Loading />}>
+        <Signature/>
         <div className="h-screen w-screen -z-10 fixed">
           <Canvas>
             <Shader />
